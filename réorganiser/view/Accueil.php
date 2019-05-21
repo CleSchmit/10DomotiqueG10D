ed277a2e4f3be3c30725350998bf63c4e4401090
CleSchmit <45144437+CleSchmit@users.noreply.github.com>
2019-05-21 09:18:20 +0200

    modif

--- réorganiser/view/Accueil.php
+++ réorganiser/view/Accueil.php
@@ -6,17 +6,17 @@ $body="<div class='main'>
             <a href=\"index.php?action=Connexion\" class='VersConnexion'>&emsp;Se Connecter&emsp;</a>
 
         <br><br><br>
-        <div class='information'>
+        <div class='information' >
             <div><p>&emsp;Vous voulez faire des économies, améliorer votre quotidien ? <br> &emsp;Vous souhaitez bénéficier de nos services</p><br><br>
             <a href=#><div class='contact'>Contactez Nous</div></a></div>
             <img src='view/images/Ask.png'>
         </div>
         
         <br>
-        <div class='information'>
-            <div><p>&emsp;Vous voulez faire des économies, améliorer votre quotidien ? <br> &emsp;Vous souhaitez bénéficier de nos services</p><br><br>
-            <a href=#><div class='contact'>Contactez Nous</div></a></div>
+        <div class='information2'>
             <img src='view/images/Ask.png'>
+            <div><p>&emsp;Vous bénéficiez de nos services <br> &emsp;vous n'avez pas de compte ?</p><br><br>
+            <a href='index.php?action=inscription'><div class='contact'>Inscrivez Vous</div></a></div>
         </div>
         <br>
         
