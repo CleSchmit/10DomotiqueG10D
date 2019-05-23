085ed7986c775bf815fc6fedff4d474e0b950d66
Charlesyyao <yyao96911@gmail.com>
2019-05-23 10:17:32 +0200

    petit change

--- réorganiser/controller/ControlCapteur2.php
+++ réorganiser/controller/ControlCapteur2.php
@@ -1,13 +1,13 @@
 <?php
 include_once 'model/function.php';
-include_once 'model/piece.php';
+include_once 'model/capteur.php';
 $bdd = bdd();
 $erreur = NULL;
 
-if(isset($_POST['Nom']) AND isset($_POST['Superficie']))
+if(isset($_POST['Nom']) AND isset($_POST['Model']))
 {
-    $piece = new piece($_POST['Nom'], $_POST['Superficie'],$_GET['id']);
-    if($piece->enregistrement()){
+    $capteur = new capteur($_POST['Nom'], $_POST['Model'], $_GET['id']);
+    if($capteur->enregistrement()){
         header("Location:index_cn.php?action=Profil#".$_GET['Maison']."");
     } else { /*Erreur lors de l'enregistrement*/
         echo '发生了错误';
