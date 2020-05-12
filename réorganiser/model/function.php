b876b4a3963a75e70659b866f0b95ac4eb6bdd7d
olivier <olivier-5@olivier5.home>
2020-05-12 16:00:57 +0200

    uu

--- réorganiser/model/function.php
+++ réorganiser/model/function.php
@@ -1,16 +1,13 @@
 <?php
 
 function bdd(){
-     try
-{
-	$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-	$bdd = new PDO('mysql:host=localhost;dbname=app;charset=utf8', 'root', '');
-}
-catch (Exception $e)
-{
-        die('Erreur : ' . $e->getMessage());
-}
-return $bdd;
+        try{
+                $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+                $bdd = new PDO('mysql:host=localhost;dbname=app;charset=utf8', 'root', '');
+        }catch (Exception $e){
+                die('Erreur : ' . $e->getMessage());
+        }
+        return $bdd;
 }
 
 ?>
